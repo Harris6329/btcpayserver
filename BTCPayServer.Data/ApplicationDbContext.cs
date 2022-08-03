32e32bbb0d0e1e9a979e364882e07d67f05eb1ef
Kukks <evilkukka@gmail.com>
2022-08-03 07:35:57 +0200

    wip

--- BTCPayServer.Data/ApplicationDbContext.cs
+++ BTCPayServer.Data/ApplicationDbContext.cs
@@ -65,6 +65,7 @@ namespace BTCPayServer.Data
         public DbSet<WebhookData> Webhooks { get; set; }
         public DbSet<LightningAddressData> LightningAddresses{ get; set; }
         public DbSet<PayoutProcessorData> PayoutProcessors { get; set; }
+        public DbSet<FormData> Forms { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -114,6 +115,7 @@ namespace BTCPayServer.Data
             LightningAddressData.OnModelCreating(builder);
             PayoutProcessorData.OnModelCreating(builder);
             //WebhookData.OnModelCreating(builder);
+            FormData.OnModelCreating(builder, Database);
 
 
             if (Database.IsSqlite() && !_designTime)
